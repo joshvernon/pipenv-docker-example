b6a65a8468bfdacb0484e6b9d5a551f7e318a663
joshvernon <jvernon_483@hotmail.com>
2018-10-05 20:44:12 -0500

    Add .dockerignore

--- sample_script.py
+++ sample_script.py
@@ -6,8 +6,10 @@ import requests
 
 print("Hello from Dockerized Python!")
 print(f"Current working directory: {os.getcwd()}")
+print(f"Current directory contents: {', '.join(os.listdir())}")
 print(f"platform.node(): {platform.node()}")
 print(f"sys.version: {sys.version}")
 print(f"sys.path: {sys.path}")
 print(f"sys.executable: {sys.executable}")
 print(f"requests version: {requests.__version__}")
+
